6b62fbe5f88eab0a0de5a03c3af91de4c10c2932
kevin579 <125067771+kevin579@users.noreply.github.com>
2024-10-05 18:56:46 -0400

    update

--- camera.py
+++ camera.py
@@ -3,7 +3,6 @@ import torch
 from torchvision import transforms, models
 from PIL import Image
 import numpy as np
-from ultralytics import YOLO
 
 
 def load_model(model_path, num_classes):
@@ -58,7 +57,6 @@ def categorize_waste(prediction):
 def main():
     # Path to your trained model
     model_path = './pythonProject1/waste_classification_model.pth'
-    yolo = YOLO('yolov8s.pt')
 
     # Define your class names in the order they were during training
     class_names = ['battery', 'biodegradable', 'biohazard', 'electronic', 'face', 'glass', 'metal', 'paper', 'plastic']
@@ -87,64 +85,32 @@ def main():
                 break
 
             frame = cv2.flip(frame, 1)
-            results = yolo.track(frame, conf=0.5)
 
             # Process the frame for prediction
-            # image_tensor = process_image(frame)
-            # prediction, confidence = predict_image(model, image_tensor, class_names)
+            image_tensor = process_image(frame)
+            prediction, confidence = predict_image(model, image_tensor, class_names)
 
-            # Apply background subtraction
-            # fgMask = backSub.apply(frame)
-
-            # Display frame count
-            # cv2.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
-            # cv2.putText(frame, str(cam.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-            # Colour
-            colour = (0, 0, 0)
+            # cv2.putText(frame, f"Confidence: {confidence}", (10, 80),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
             # Display prediction
-            for result in results:
-                for box in result.boxes:
-                    # print(box.conf[0])
-                    # if box.conf[0] > 0.3:
-                    # get coordinates
-                    [x1, y1, x2, y2] = box.xyxy[0]
-                    # convert to int
-                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            if float(confidence) > 80:
+                cv2.putText(frame, f"Prediction: {prediction}", (10, 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                item = prediction
+                possible[item] = possible.get(item, 0) + confidence
+                type_of_waste = categorize_waste(prediction)
+            elif float(confidence) > 70:
+                cv2.putText(frame, f"Prediction: {item}", (10, 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            else:
+                cv2.putText(frame, f"Prediction: {item}", (10, 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-                    origin = (x1, y1)
 
-                    # make rectangle
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), colour, 2)
-                    img = frame[y1:y2, x1:x2]
-                    image_tensor = process_image(img)
-                    prediction, confidence = predict_image(model, image_tensor, class_names)
-                    # Process the frame for prediction
-                    # cv2.imshow("cropped", img)
-                    if float(confidence) > 85:
-                        cv2.putText(frame, f"Prediction: {prediction}", origin,
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, colour, 2)
-                        # cv2.putText(img, f"Confidence: {confidence:.2f}%", origin,
-                        #         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                        item = prediction
-                        possible[item] = possible.get(item, 0) + confidence
-                        type_of_waste = categorize_waste(prediction)
-                    elif float(confidence) > 50:
-                        cv2.putText(frame, f"Prediction: {item}", origin,
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, colour, 2)
-                        # cv2.putText(img, f"Confidence: {confidence:.2f}%", origin,
-                        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                    else:
-                        cv2.putText(frame, f"No Item Detected", origin,
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, colour, 2)
-                        # cv2.putText(img, f"Confidence: {confidence:.2f}%", origin,
-                        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                    cv2.putText(frame, f"Type: {type_of_waste}", (x1, y1 - 20),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, colour, 2)
-
+            cv2.putText(frame, f"Type: {type_of_waste}", (10, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             # Display the captured frame
-            # cv2.imshow("Filtered", fgMask)
             cv2.imshow(window_name, frame)
 
             # Check if window has been closed
